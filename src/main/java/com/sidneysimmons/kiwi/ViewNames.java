44b564741c069e628717f74b677f4c70806e01a5
Sidney Simmons <sas.simm@gmail.com>
2020-06-08 16:56:48 -0600

    Separate nav into generic "nav.html" file

--- src/main/java/com/sidneysimmons/kiwi/ViewNames.java
+++ src/main/java/com/sidneysimmons/kiwi/ViewNames.java
@@ -7,6 +7,7 @@ package com.sidneysimmons.kiwi;
  */
 public class ViewNames {
 
+    public static final String MUSICIAN = "musician";
     public static final String SOFTWARE_ENGINEER = "software-engineer";
 
     private ViewNames() {
---
+++ src/main/java/com/sidneysimmons/kiwi/musician/controller/MusicianController.java
@@ -0,0 +1,22 @@
+package com.sidneysimmons.kiwi.musician.controller;
+
+import com.sidneysimmons.kiwi.ViewNames;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Musician controller.
+ * 
+ * @author Sidney Simmons
+ */
+@Controller
+@RequestMapping(value = "/musician")
+public class MusicianController {
+
+    @GetMapping(value = "")
+    public String musician() {
+        return ViewNames.MUSICIAN;
+    }
+
+}
