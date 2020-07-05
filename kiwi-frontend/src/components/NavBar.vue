2e465a4dce826ae9c5c354287f51e3d21d146425
Sidney Simmons <sas.simm@gmail.com>
2020-07-05 17:22:04 -0600

    Implement nav bar as a shared vue component

---
+++ kiwi-frontend/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">Sidney Simmons</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a class="nav-link" :class="currentPage === 'software-engineer' ? 'active' : ''" href="/software-engineer">Software Engineer</a></li>
+                    <li class="nav-item"><a class="nav-link" :class="currentPage === 'contact' ? 'active' : ''" href="/contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    props: {
+        currentPage: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- kiwi-frontend/src/pages/software-engineer/SoftwareEngineer.vue
+++ kiwi-frontend/src/pages/software-engineer/SoftwareEngineer.vue
@@ -1,27 +1,15 @@
 <template>
-  <div class="software-engineer">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">Sidney Simmons</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active"><a class="nav-link" href="/software-engineer">Software Engineer</a></li>
-                <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
-            </ul>
-        </div>
-    </nav>
-  </div>
+    <div>
+        <NavBar currentPage="software-engineer"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from '../../components/HelloWorld.vue'
+import NavBar from '../../components/NavBar.vue'
 export default {
   name: 'SoftwareEngineer',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
