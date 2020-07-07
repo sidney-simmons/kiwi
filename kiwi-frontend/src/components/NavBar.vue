d7ad3d00686cf7f922906b0524c764870b8a2619
Sidney Simmons <sas.simm@gmail.com>
2020-07-06 18:11:06 -0600

    Move nav related css to the nav bar vue component

--- kiwi-frontend/src/components/NavBar.vue
+++ kiwi-frontend/src/components/NavBar.vue
@@ -1,18 +1,16 @@
 <template>
-    <div>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-            <a class="navbar-brand" href="/">Sidney Simmons</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link" :class="currentPage === 'software-engineer' ? 'active' : ''" href="/software-engineer">Software Engineer</a></li>
-                    <li class="nav-item"><a class="nav-link" :class="currentPage === 'contact' ? 'active' : ''" href="/contact">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
-    </div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Sidney Simmons</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"><a class="nav-link" :class="currentPage === 'software-engineer' ? 'active' : ''" href="/software-engineer">Software Engineer</a></li>
+                <li class="nav-item"><a class="nav-link" :class="currentPage === 'contact' ? 'active' : ''" href="/contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -25,5 +23,7 @@ export default {
 </script>
 
 <style scoped>
-
+nav {
+    margin-bottom: 15px;
+}
 </style>
