465589487ce45a6b4ced8ec44f3f23f99a79a1a7
Sidney Simmons <sas.simm@gmail.com>
2020-07-20 17:27:38 -0600

    Send a google analytics event any time an AJAX request fails

--- kiwi-frontend/src/services/AxiosService.js
+++ kiwi-frontend/src/services/AxiosService.js
@@ -1,12 +1,22 @@
 import Axios from "axios";
+import GoogleAnalyticsService from "@/services/GoogleAnalyticsService";
 
 Axios.interceptors.response.use(
     function (response) {
-        // All happy 2XX responses
+        // All happy 2XX responses - return the response
         return response;
     },
     function (error) {
-        // All other sad responses
+        // All other sad responses - send a google analytics event and return the error
+        try {
+            let responsePath = error.response.data.path;
+            let responseStatus = error.response.data.status;
+            if (responsePath && responseStatus) {
+                GoogleAnalyticsService.triggerGoogleAnalyticsEvent("errors", "ajax-response-error", responsePath + " - " + responseStatus);
+            }
+        } catch (tryCatchError) {
+            // Nothing to do here
+        }
         return Promise.reject(error);
     }
 );
